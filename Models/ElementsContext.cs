cdec42e874a2d2d9f901dad2a9b9594c008b6587
Alex Moroz <alex.moroz@outlook.de>
2020-11-03 19:00:15 +0100

    Added data models

---
+++ Models/ElementsContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace LocksSearch.Models
+{
+    public class ElementsContext : DbContext
+    {
+        public DbSet<Building> Buildings { get; set; }
+        public DbSet<Lock> Locks { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Media> Medias { get; set; }
+    }
+
+    public class Building
+    {
+        public Guid id;
+        public string shortCut;
+        public string name;
+        public string description;
+
+        public List<Lock> Locks = new List<Lock>();
+    }
+
+    public class Lock
+    {
+        public Guid id;
+        public Building building;
+        public string type;
+        public string name;
+        public string description;
+        public string serialNumber; 
+        public string floor;
+        public string roomNumber;
+    }
+
+    public class Group
+    {
+        public Guid id;
+        public string name;
+        public string description;
+
+        public List<Media> Medias = new List<Media>();
+    }
+
+    public class Media
+    {
+        public Guid id;
+        public Group group;
+        public string type;
+        public string owner;
+        public string description;
+        public string serialNumber;
+    }
+}
---
+++ Models/WeightsData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LocksSearch.Models
+{
+    internal static class WeightsData
+    {
+        public static Dictionary<string, int> Building = new Dictionary<string, int>
+        {
+            {"shortCut", 7},
+            {"name", 9},
+            {"decsription", 5}
+        };
+
+        public static Dictionary<string, int> Lock = new Dictionary<string, int>
+        {
+            {"type", 3 },
+            {"name", 10},
+            {"serialNumber", 8},
+            {"floor", 6},
+            {"roomNumber", 6},
+            {"decsription", 6}
+        };
+
+        public static Dictionary<string, int> Group = new Dictionary<string, int>
+        {
+            {"name", 9},
+            {"decsription", 5}
+        };
+
+        public static Dictionary<string, int> Media = new Dictionary<string, int>
+        {
+            {"type", 3},
+            {"owner", 10},
+            {"serialNumber", 8},
+            {"decsription", 6}
+        };
+    }
+}
